ac8ff312389cc98c35ee290a05830b2dd64b5b4b
kawsar9990 <mdhossian72@gmail.com>
2025-08-29 23:20:13 +0600

    first commit

--- src/Page/HomePage/landing.jsx
+++ src/Page/HomePage/landing.jsx
@@ -1,4 +1,5 @@
 import Home from "./home"
+import Chooseus from "./chooseus"
 
 
 
@@ -9,6 +10,7 @@ export default function Landingpage(){
 
 <div id="home">
      <Home />
+     <Chooseus />
 </div>
 
          </div>
